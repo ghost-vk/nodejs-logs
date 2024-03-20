47ed5e5b84dde31634cf9839c37f75db5ebbd535
Vlad Kuznetsov <ghost.vkv@gmail.com>
2024-03-20 12:13:23 +0300

    setup eslint

--- packages/nest/.eslintrc.js
+++ packages/nest/.eslintrc.js
@@ -1,25 +1,68 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
   env: {
     node: true,
-    jest: true,
+  },
+  extends: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
   },
   ignorePatterns: ['.eslintrc.js'],
+  plugins: ['import', 'simple-import-sort', 'unused-imports'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'simple-import-sort/imports': 1,
+    'simple-import-sort/exports': 1,
+    'import/first': 1,
+    'import/newline-after-import': 1,
+    'import/no-duplicates': 1,
+    'import/no-absolute-path': 1,
+    'unused-imports/no-unused-imports': 1,
   },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: 'tsconfig.eslint.json',
+        tsconfigRootDir: __dirname,
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'prettier',
+      ],
+      plugins: ['@typescript-eslint', 'import', 'simple-import-sort'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': 2,
+        '@typescript-eslint/no-inferrable-types': 2,
+        '@typescript-eslint/explicit-function-return-type': 2,
+        '@typescript-eslint/no-empty-interface': 2,
+        '@typescript-eslint/no-explicit-any': 2,
+        'padding-line-between-statements': [
+          'error',
+          {
+            blankLine: 'always',
+            prev: '*',
+            next: 'return',
+          },
+          {
+            blankLine: 'always',
+            prev: '*',
+            next: 'throw',
+          },
+          {
+            blankLine: 'always',
+            prev: ['const', 'let', 'var'],
+            next: '*',
+          },
+          {
+            blankLine: 'any',
+            prev: ['const', 'let', 'var'],
+            next: ['const', 'let', 'var'],
+          },
+        ],
+      },
+    },
+  ],
 };
